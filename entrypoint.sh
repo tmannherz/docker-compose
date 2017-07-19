4ee94e46835845dae52af5639e90dfbd372747f6
Todd Mannherz <todd.mannherz@gmail.com>
2017-07-19 13:35:24 -0600

    Entrypoint improvements to allow other commands.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,14 @@ set -e
 echo "Authenticating with $AUTH_URL"
 docker login -u _json_key -p "$(cat service-account.json)" "$AUTH_URL"
 
-exec docker-compose "$@"
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+    set -- docker-compose "$@"
+fi
+
+# if our command is a valid Docker Compose subcommand, invoke it through docker-compose
+if docker-compose help "$1" > /dev/null 2>&1; then
+    set -- docker-compose "$@"
+fi
+
+exec "$@"
